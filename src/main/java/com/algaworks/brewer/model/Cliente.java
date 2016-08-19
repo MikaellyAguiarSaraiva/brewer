c82e5414f4be8f5ffa5f45e7376b1bc402c314c3
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-08-18 23:42:57 -0300

    Desafio Prototipando o cadastro de cliente

---
+++ src/main/java/com/algaworks/brewer/model/Cliente.java
@@ -0,0 +1,105 @@
+package com.algaworks.brewer.model;
+
+public class Cliente {
+
+	private String nome;
+	private String tipoPessoa;
+	private String cpfCnpj;
+	private String telefone;
+	private String email;
+	private String lagradouro;
+	private String numero;
+	private String complemento;
+	private String cep;
+	private String estado;
+	private String cidade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+	public void setTipoPessoa(String tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLagradouro() {
+		return lagradouro;
+	}
+
+	public void setLagradouro(String lagradouro) {
+		this.lagradouro = lagradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+}
