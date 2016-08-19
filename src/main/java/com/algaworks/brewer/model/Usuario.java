f07c255716f79b877016a0e7c00729ece45e271c
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-08-19 00:06:08 -0300

    Desafio prototipando o cadastro de usuário

---
+++ src/main/java/com/algaworks/brewer/model/Usuario.java
@@ -0,0 +1,59 @@
+package com.algaworks.brewer.model;
+
+public class Usuario {
+	private String nome;
+	private String email;
+	private String dataNascimento;
+	private String senha;
+	private String status;
+	private String grupo;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+
+}
