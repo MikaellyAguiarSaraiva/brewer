e7d0fc5b48abdfbf30fff51398f58000ddca2abe
Mikaelly Aguiar <mikaelly-aguiar@hotmail.com>
2016-09-29 21:59:47 -0300

    Convertendo valores numéricos

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,13 @@
 $(function() {
 	var decimal = $('.js-decimal');
-	decimal.maskMoney();
+	decimal.maskMoney({
+		decimal : ',',
+		thousands : '.',
+	});
 	
 	var plain = $('.js-plain');
-	plain.maskMoney({ precision: 0 });
+	plain.maskMoney({ 
+		precision: 0,
+		thousands : '.',
+	});
 });
