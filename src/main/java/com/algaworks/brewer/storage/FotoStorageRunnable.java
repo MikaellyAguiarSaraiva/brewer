bf5c4e6f732ad5126a323418ff1ee4d9d5bdffed
Mikaelly Aguiar Saraiva <mikaelly-aguiar@hotmail.com>
2016-10-11 17:50:14 -0300

    Criando pastas para salvar fotos.

--- src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
+++ src/main/java/com/algaworks/brewer/storage/FotoStorageRunnable.java
@@ -9,17 +9,20 @@ public class FotoStorageRunnable implements Runnable {
 
 	private MultipartFile[] files;
 	private DeferredResult<FotoDTO> resultado;
+	private FotoStorage fotoStorage;
 
-	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado) {
+	public FotoStorageRunnable(MultipartFile[] files, DeferredResult<FotoDTO> resultado, FotoStorage fotoStorage) {
 		super();
 		this.files = files;
 		this.resultado = resultado;
+		this.fotoStorage = fotoStorage;
 	}
 
 	@Override
 	public void run() {
-		System.out.println(">>>> files : " + files[0].getSize());
-		//TODO: Salvar a foto no sistema de arquivos...
+		//TODO: Salvar a foto no sistema de arquivos [TEMPORARIO]
+		this.fotoStorage.salvarTemporariamente(files);
+		
 		String nomeFoto = files[0].getOriginalFilename();
 		String contentType = files[0].getContentType();
 		
